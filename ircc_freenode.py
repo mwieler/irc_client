909b587ce08ef6566e7a4fd470e0a5de9fb04281
mwieler <mw166301@gmail.com>
2012-12-04 17:12:09 -0500

    Same functionality, now implemented using classes.

--- ircc_freenode.py
+++ ircc_freenode.py
@@ -17,80 +17,99 @@ PORT = 6667
 CHANNEL = '#HACKERSCHOOL'
 NICK = 'testmatt'
 
-IRC_CMDS = {'m':'PRIVMSG','n':'NICK', 'j':'JOIN','q':'QUIT'}
+IRC_CMDS = {'m':'PRIVMSG','n':'NICK', 'j':'JOIN','q':'QUIT','NOTICE':'notice'}
 
+# Create 'sock', a socket object
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create IPv4, TCP socket
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #reuse port quickly
+
+class message:
+    def __init__(self,(msg,source)): #remember init takes arguments, NOT the class
+        self.original_msg = msg
+        self.source = source
+        self.parsed_msg = None
+        self.error_attributes = (False,"Message has not yet been parsed by parse_message")
+        self.IRC_formatted_msg = None
+                        
+    def parse_msg(self): #returns ((prefix,command,args,trailer),(parseable_msg,why_unparseable)
+        if self.original_msg[0] == '/' and self.source == 'user': #/ indicates a user command
+           user_cmd, args_and_trailer = self.original_msg[1:].split(' ',1)
+           args, trailer = args_and_trailer.split(' ',1) #for now, only one-argument functions can be handled
+           args = args.strip()
+           self.parsed_msg = (None, user_cmd, args, trailer)
+           self.error_attributes = (True,None)
+    
+        elif self.source == 'server':
+            if self.original_msg[0] == ':': 
+                prefix, not_prefix = self.original_msg[1:].split(' ',1) #splits message into pre-space and post-space
+            elif self.original_msg[0] != ':': #this statement is redundantly explicit, for clarity
+                prefix, not_prefix = None, self.original_msg
+            command_and_args, trailer = not_prefix.split(':',1)
+            print "original_msg: ",msg_object.original_msg
+            print "parsed_msg: ",msg_object.parsed_msg
+            print "\nParsed message:\n", msg_object.parsed_msg #placeholder. eventually display properly formatted message
+            print "command and args\n",command_and_args,"\nend of command and args" #debug stmt
+            #pdb.set_trace() #debug statement
+            command, args = command_and_args.split(' ',1) #returns '' for args if no args exist
+            args = args.rstrip() #remove trailing whitespace. Should args be a space-delimited string or a list?
+            self.parsed_msg = (prefix, command, args, trailer)
+            self.error_attributes = (True, None)
+        else:
+           self.parsed_msg = None
+           self.error_attributes = (False, "Sorry, parse_msg couldn't handle the message")
+
+    def format_as_IRC(self): #check message is valid prior to running
+        #pdb.set_trace()
+        (prefix, user_cmd, args, trailer) = self.parsed_msg
+        
+        if not IRC_CMDS[user_cmd]:
+            print "Sorry, your command was not recognized"
+        else:
+            if prefix:
+                self.IRC_formatted_msg = ":"+prefix+" "+IRC_CMDS[user_cmd]+" "+args+" :"+trailer+"\n" #the \n is necessary for the IRC server
+            if not prefix:
+                self.IRC_formatted_msg = IRC_CMDS[user_cmd]+" "+args+" :"+trailer+"\n" #the \n is necessary for the IRC server
+
 
 def read(sock): #listens for and prints out the received message
     while True:
         msg = sock.recv(1024)
-        if msg:
-            print msg #not sure why the quotes are required
-            msgtuple = (msg,'server')
-            (validmsg, (prefix,command,args,trailer),errormsg, source) = parse_msg(*msgtuple)
-            if command == 'PING':
-                sock.sendall('PONG '+trailer)
-            #print 'Server message was parsed as ',parse_servermsg(msg)
-
-def parse_msg(msg,source): #returns (validmsg, (prefix,command,args,trailer),errormsg, source)
-    #validmsg = False #better way to construct valid-message checking?
-    if msg[0] == '/': #user command
-       user_cmd, args_and_trailer = msg[1:].split(' ',1)
-       args, trailer = args_and_trailer.split(' ',1) #for now, only one-argument functions can be handled
-       args = args.strip()
-       #args = args.split(' ') #Q: will whitespace get in the way?  
-       #args = ' '.join(args) #joins the elements of list in 'args' with spaces
-       return (True,(None,IRC_CMDS[user_cmd], args, trailer), None, source)    
-    elif msg[0] == ':':
-        prefix, not_prefix = msg[1:].split(' ',1) #splits message into pre-space and post-space
-        command_and_args, trailer = not_prefix.split(':',1)
-        command, args = command_and_args.split(' ',1) #returns '' for args if no args exist
-        args = args.rstrip() #remove trailing whitespace
-        return (True,(prefix, command, args, trailer), None, source)
-    else:
-       return (False,None,"Sorry, parse_msg couldn't handle the message")
-
-#DEBUGGING STATEMENTS
-print parse_msg('/m testmatt testmsg','user')
-assert parse_msg('/m testmatt testmsg','user') == (True,(None,'PRIVMSG','testmatt','testmsg'),None,'user')
-
-print parse_msg(':rajaniemi.freenode.net 433 * mattwieler :Nickname is already in use.','server')
-assert parse_msg(':rajaniemi.freenode.net 433 * mattwieler :Nickname is already in use.','server')
-
-def parsed_to_irc(validmsg,(prefix,IRC_command,args,trailer),errormsg, source):
-    if validmsg:
-        return (True,IRC_command+" "+args+" :"+trailer+"\n",None) #the \n is necessary for the IRC server
-    if not validmsg: #if not a valid 
-        return (False,None,"Usage: /command arg trailer2")
-
-#debugging
-# a = parse_servermsg(':rajaniemi.freenode.net 433 * mattwieler :Nickname is already in use.')
-# b = parse_servermsg('PING :gibson.freenode.net')
+        #msg_object = message((msg,'server'))
+        print msg
+        #pdb.set_trace()
+        #msg_object.parse_msg() #we should be able to combine these two lines, or automatically call them
+        #msg_object.format_as_IRC() #we should be able to combine these two lines, or automatically call them
+        # parseable_msg, why_unparseable = msg_object.error_attributes
+        # if parseable_msg:
+        #     if msg_object.parsed_msg[1] == 'PING': #clean up - indexing refers to IRC command. make this clear
+        #         sock.sendall('PONG '+trailer)
+        # if not parseable_msg:
+        #     print msg_object.original_msg, "was not parseable. Reason:\n", why_unparseable
 
 def write(sock): #blocks on raw_msg, returns any msg from user
     while True:
         msg = raw_input("What to send to server?")
         msgtuple = (msg,'user') #(message,source)
-        parsed_msg = parse_msg(*msgtuple)
-        validmsg,IRC_msg,errormsg = parsed_to_irc(*parsed_msg)
-        if validmsg:
-            print "Sending",IRC_msg
-            sock.sendall(IRC_msg)
+        msg_object2 = message(msgtuple)
+        msg_object2.parse_msg() #these two fcns should be run automatically?
+        #these two fcns should be run automatically?
+        #pdb.set_trace()
+        if msg_object2.error_attributes[0]: #if the message has been parsed...
+            msg_object2.format_as_IRC() #format it as an IRC message....
+            print "Sending",msg_object2.IRC_formatted_msg #and send it
+            sock.sendall(msg_object2.IRC_formatted_msg)
         else:
-            print errormsg
+            print msg_object2.error_attributes[1] #make this clearer
 
-read_thread = threading.Thread(target=read, args=(sock,)) #starts read thread by passing
-# read function and its arguments
-read_thread.daemon = True #if only daemon threads are still running, they'll die.
+read_thread = threading.Thread(target=read, args=(sock,)) #starts read thread by passing read function and its arguments
+read_thread.daemon = True #if only daemon threads are still running, they'll die. So if you kill the main loop
 
 def main(): 
     print "test to see if main function is run"
-    print "2"
     print "Connecting to",SERVER,"Port",PORT,"as",NICK
     sock.connect((SERVER,PORT))
-    print "Waiting 3 seconds..." #this should only be sent once the server replies with stuff
-    time.sleep(3)
+    print "Waiting 2 seconds..." #this should only be sent once the server replies with stuff
+    time.sleep(2)
     print "Sending nickname request to server..."
     sock.send("NICK "+NICK+"\nUSER "+NICK+" 0 * "+NICK+"\n")
     read_thread.start()
@@ -100,7 +119,7 @@ def main():
 if __name__ == '__main__':
      main()
 
-     # while True:
+    # while True:
     #     msg = raw_msg("thoughts?")
     #     if msg == 'quit':
     #             break
@@ -113,11 +132,18 @@ if __name__ == '__main__':
     #         print "Unanticipated exception encountered."
 
 
+#DEBUGGING STATEMENTS
+#print parse_msg('/m testmatt testmsg','user')
+#assert parse_msg('/m testmatt testmsg','user') == ((None,'PRIVMSG','testmatt','testmsg'),None,'user')
+
+#print parse_msg(':rajaniemi.freenode.net 433 * mattwieler :Nickname is already in use.','server')
+#assert parse_msg(':rajaniemi.freenode.net 433 * mattwieler :Nickname is already in use.','server')
+
+# a = parse_servermsg(':rajaniemi.freenode.net 433 * mattwieler :Nickname is already in use.')
+# b = parse_servermsg('PING :gibson.freenode.net')
+#------------------------------------------------------
+
 #sockob = handle_new_connection(lsock) #creates a sockob that can .sendall and .recv
-
-
-
-
 
 #sock.bind(('0.0.0.0', 4567)) #make socket available on local port 4567
 
