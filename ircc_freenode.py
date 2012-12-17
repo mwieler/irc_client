8c058b73c7ea6746b7ae4b2d29f6602acd423d04
mwieler <mw166301@gmail.com>
2012-12-17 11:53:33 -0500

    Working pygame interface

--- ircc_freenode.py
+++ ircc_freenode.py
@@ -2,7 +2,8 @@
 # usage: nc 127.0.0.1 4567
 
 # To Do:
-# 1. Trailers are OPTIONAL
+# 1. make trailers are OPTIONAL -- DONE
+# 2. add interactive pygame interface to learn about control structures, user interface
 
 
 # 1. Add ability to join a channel.
@@ -12,6 +13,12 @@ import socket
 import threading
 import pdb
 import time
+import select
+
+#for pygame
+import sys
+import pygame
+from pygame.locals import *
 
 # rather than if use dict. to turn user-supplied command into IRC command
 
@@ -20,33 +27,34 @@ PORT = 6667
 CHANNEL = '#HACKERSCHOOL'
 NICK = 'testmatt'
 
-IRC_CMDS = {'m':'PRIVMSG','n':'NICK', 'j':'JOIN','q':'QUIT','NOTICE':'notice'}
+IRC_CMDS = {'m':'PRIVMSG','n':'NICK', 'j':'JOIN','q':'QUIT','o':'NOTICE'}
 
-# Create 'sock', a socket object
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create IPv4, TCP socket
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #reuse port quickly
+def print_dict():
+    print "User command   |IRC Command\n-----------------------------"
+    for k,v in IRC_CMDS.items():
+        print k,"\t\t",v
 
-class message:
+class Message:
     def __init__(self,(msg,source)): #remember init takes arguments, NOT the class
-        self.original_msg = msg
+        self.original_msg = '/'+msg #pygame may not recognize / so for now add it in manually
         self.source = source
         self.parsed_msg = None
-        self.error_attributes = (None,"Message has not yet been parsed by parse_message")
+        self.valid_msgtuple = (None,"Message has not yet been parsed by parse_message")
         self.IRC_formatted_msg = None
     
     def is_msg_in_expected_structure(self):
         #pdb.set_trace()
         if self.original_msg[0] != '/':
-            self.error_attributes = (False, "string must begin with a '/'")
+            self.valid_msgtuple = (False, "commands must begin with a '/'")
         elif len(self.original_msg.split(' ')) < 2:
-            self.error_attributes = (False, "usage: /command argument trailer")
+            self.valid_msgtuple = (False, "usage: /command argument trailer")
         elif not IRC_CMDS.get(self.original_msg[1:].split(' ')[0],None): #returns None if user_cmd key not found in IRC_CMDS dict
-            self.error_attributes = (False,"User command not recognized")
+            self.valid_msgtuple = (False,"User command not recognized. Valid IRC commands include:",print_dict())
         else:
-            self.error_attributes = (True, None)
+            self.valid_msgtuple = (True, None)
 
     def parse_msg(self): #returns ((prefix,command,args,trailer),(parseable_msg,why_unparseable)
-        if self.original_msg[0] == '/' and self.source == 'user' and self.error_attributes[0]: #/ indicates a user command
+        if self.original_msg[0] == '/' and self.source == 'user' and self.valid_msgtuple[0]: #/ indicates a user command
             user_cmd, args_and_trailer = self.original_msg[1:].split(' ',1)
             #pdb.set_trace()
             if len(args_and_trailer.split(' ')) > 1:
@@ -70,10 +78,10 @@ class message:
             command, args = command_and_args.split(' ',1) #returns '' for args if no args exist
             args = args.rstrip() #remove trailing whitespace. Should args be a space-delimited string or a list?
             self.parsed_msg = (prefix, command, args, trailer)
-            self.error_attributes = (True, None)
+            self.valid_msgtuple = (True, None)
         else:
            self.parsed_msg = None
-           #self.error_attributes = (False, "Sorry, parse_msg couldn't handle the message")
+           #self.valid_msgtuple = (False, "Sorry, parse_msg couldn't handle the message")
            #pdb.set_trace()
 
     def format_as_IRC(self): #check message is valid prior to running
@@ -88,86 +96,102 @@ class message:
 def read(sock): #listens for and prints out the received message
     while True:
         msg = sock.recv(1024)
-        #msg_object = message((msg,'server'))
+        #msg_object = Message((msg,'server'))
         print msg #for now, all the client does with servers messages is print them: eventually, it should make sense of them, too, and pong the server
         #pdb.set_trace()
         #msg_object.parse_msg() #we should be able to combine these two lines, or automatically call them
         #msg_object.format_as_IRC() #we should be able to combine these two lines, or automatically call them
-        # parseable_msg, why_unparseable = msg_object.error_attributes
+        # parseable_msg, why_unparseable = msg_object.valid_msgtuple
         # if parseable_msg:
         #     if msg_object.parsed_msg[1] == 'PING': #clean up - indexing refers to IRC command. make this clear
         #         sock.sendall('PONG '+trailer)
         # if not parseable_msg:
         #     print msg_object.original_msg, "was not parseable. Reason:\n", why_unparseable
 
-def write(sock): #blocks on raw_msg, returns any msg from user
-    while True:
-        msg = raw_input("What to send to server?")
-        msgtuple = (msg,'user') #(message,source)
-        msg_object2 = message(msgtuple)
-        msg_object2.is_msg_in_expected_structure()
-        msg_object2.parse_msg() #these two fcns should be run automatically?
-        #these two fcns should be run automatically?
-        #pdb.set_trace()
-        if msg_object2.error_attributes[0]: #if the message has been parsed...
-            msg_object2.format_as_IRC() #format it as an IRC message....
-            print "Sending",msg_object2.IRC_formatted_msg #and send it
-            sock.sendall(msg_object2.IRC_formatted_msg)
-        else:
-            print msg_object2.error_attributes[1] #make this clearer
+def write(sock,msg): #blocks on raw_msg, returns any msg from user
+    msgtuple = (msg,'user') #(message,source)
+    user_msg = Message(msgtuple)
+    user_msg.is_msg_in_expected_structure()
+    user_msg.parse_msg()
+    if user_msg.valid_msgtuple[0]: #if the message has been parsed successfully...
+        user_msg.format_as_IRC() #format it as an IRC message....
+        print "Sending",user_msg.IRC_formatted_msg #and send it
+        sock.sendall(user_msg.IRC_formatted_msg)
+    else:
+        print user_msg.valid_msgtuple[1] #make this clearer
 
-read_thread = threading.Thread(target=read, args=(sock,)) #starts read thread by passing read function and its arguments
-read_thread.daemon = True #if only daemon threads are still running, they'll die. So if you kill the main loop
-
-def main(): 
-    print "test to see if main function is run"
+def setup_sock():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create IPv4, TCP socket
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #reuse port quickly 
     print "Connecting to",SERVER,"Port",PORT,"as",NICK
     sock.connect((SERVER,PORT))
-    print "Waiting 2 seconds..." #this should only be sent once the server replies with stuff
-    time.sleep(2)
+    sock.setblocking(False)
+#    print "Waiting a few seconds..." #this should only be sent once the server replies with stuff
+#    time.sleep(5)
     print "Sending nickname request to server..."
     sock.send("NICK "+NICK+"\nUSER "+NICK+" 0 * "+NICK+"\n")
-    read_thread.start()
-    write(sock) #enters a while loop. If the loop is broken, only the thread remains, and it dies
+    return sock
+
+def main(): 
+    # Create 'sock', a socket object
+    user_message = ""
+    server_message = ''
+    sock = setup_sock()
+
+    #set up pygame interface
+    screen = pygame.display.set_mode((1080,640)) #width, height of canvas
+   
+    #initialize the rectangles
+    rect = pygame.draw.rect(screen,(255,155,55),(0,0,10,10)) #initializes rectangle (horiz,vert,width,heigh)
+    rect.bottomleft = (0,550) #moves bottom-left corner of rectangle to new coordinate (horiz, vert)
+
+    rect2 = pygame.draw.rect(screen,(153,33,255),(0,0,10,10))
+    rect2.bottomleft = (0,90)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN: #if the user depresses a key
+                if event.key == K_RETURN: #if the enter key is depressed (event.key == 13) 
+                    write(sock,user_message)
+                    user_message = '' #this line seems to not work, not sure why not
+
+                if event.key == K_BACKSPACE:
+                    user_message = user_message[:-1]
+                else:
+                    user_message += event.unicode
+        
+        readable, writeable, error = select.select([sock], [sock], [sock], 0)
+        
+        screen.fill((0,0,0))
+        
+        usermsg_fontobject = pygame.font.Font(None,72) #makes a font object
+        textsurface = usermsg_fontobject.render(user_message,1,(255,255,255)) #not sure exactly
+
+        servermsg_fontobject2 = pygame.font.Font(None, 24)
+        textsurface2 = servermsg_fontobject2.render(server_message,1,(255,255,255)) #not sure exactly
+
+        if readable:
+            #server_message2 = server_message
+            fontpixelheight = servermsg_fontobject2.get_linesize()
+            rect2.move(0,-fontpixelheight*100)
+            server_message = "\n"+sock.recv(1024)
+            textsurface2 = servermsg_fontobject2.render(server_message,1,(255,255,255))
+
+        #re-draw the rectangle
+        rect = pygame.draw.rect(screen,(255,155,55),rect) #this re-draws the rectangle (now that it's been moved)
+        rect2 = pygame.draw.rect(screen,(153,33,255),rect2) #this re-draws the rectangle (now that it's been moved)
+
+        screen.blit(textsurface,(0,540)) #screen has method blit, which can put, in this case, textsurface onto rect
+        screen.blit(textsurface2,rect2)
+
+        pygame.display.flip() #updates display
+
 
 
 if __name__ == '__main__':
-     main()
-
-    # while True:
-    #     msg = raw_msg("thoughts?")
-    #     if msg == 'quit':
-    #             break
-    #     validmsg, msg_IRC, errormsg = parse(msg)
-    #     if validmsg:
-    #         sock.sendall(msg_IRC)
-    #     elif not validmsg:
-    #         print errormsg
-    #     else:
-    #         print "Unanticipated exception encountered."
-
-
-#DEBUGGING STATEMENTS
-#print parse_msg('/m testmatt testmsg','user')
-#assert parse_msg('/m testmatt testmsg','user') == ((None,'PRIVMSG','testmatt','testmsg'),None,'user')
-
-#print parse_msg(':rajaniemi.freenode.net 433 * mattwieler :Nickname is already in use.','server')
-#assert parse_msg(':rajaniemi.freenode.net 433 * mattwieler :Nickname is already in use.','server')
-
-# a = parse_servermsg(':rajaniemi.freenode.net 433 * mattwieler :Nickname is already in use.')
-# b = parse_servermsg('PING :gibson.freenode.net')
-#------------------------------------------------------
-
-#sockob = handle_new_connection(lsock) #creates a sockob that can .sendall and .recv
-
-#sock.bind(('0.0.0.0', 4567)) #make socket available on local port 4567
-
-#sock.listen(1) #transforms socket into 'listening' socket -- now functions
-    # as a switchboard operator that can accept new connection requests
-
-# def handle_new_connection(lsock):
-#     sockob, addr = lsock.accept() #returns socket obecty, address (IP, PORT)
-#     lsock.setblocking(False) #makes the socket nonblocking
-#     print 'Connection accepted from ', addr #debug stmt
-#     print 'Socket connects', sockob.getsockname(), 'with peer', sockob.getpeername() #debug stmt
-#     return sockob #returns a socket obect that can send via sockob.sendall and receive via sockob.recv
+    pygame.init()
+    main()
+    # import inputbox2 as ibox
+    # screen = ibox.pygame.display.set_mode((1080,240))
+    # if ibox.ask(screen, "input>  "):
+    #     print "'%s' was entered." % ibox.ask(screen, "input>  ")
